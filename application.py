a3a0d12c2242aafc91bdd59ff11c10855a22e4b8
WengChaoxi <49865983+WengChaoxi@users.noreply.github.com>
2021-03-07 17:14:44 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import Flask
+from flask import Flask as FlaskBase
 from flask_sqlalchemy import SQLAlchemy
 from flask_apscheduler import APScheduler
 import os
@@ -9,9 +9,9 @@ from common.libs.utils import correctPath
 db = SQLAlchemy()
 scheduler = APScheduler() # 定时任务
 
-class App(Flask):
+class Flask(FlaskBase):
     def __init__(self, import_name, static_folder, template_folder, root_path):
-        super(App, self).__init__(import_name, static_folder=static_folder, template_folder=template_folder, root_path=root_path)
+        super(Flask, self).__init__(import_name, static_folder=static_folder, template_folder=template_folder, root_path=root_path)
         
         self.config.from_pyfile(correctPath('config/config.py'))
         db.init_app(self)
@@ -22,7 +22,7 @@ class App(Flask):
 
 static_path = correctPath('web/static')
 templates_path = correctPath('web/templates')
-app = App(__name__, static_folder=static_path, template_folder=templates_path, root_path=os.getcwd())
+app = Flask(__name__, static_folder=static_path, template_folder=templates_path, root_path=os.getcwd())
 
 from flask_cors import CORS
 CORS(app, supports_credentials = True) # 解决跨域问题
