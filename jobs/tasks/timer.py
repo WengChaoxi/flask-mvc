261954c5e63150e4eb91a02725574913a9d8c16f
WengChaoxi <49865983+WengChaoxi@users.noreply.github.com>
2021-03-09 11:00:43 +0800

    Update timer.py

--- jobs/tasks/timer.py
+++ jobs/tasks/timer.py
@@ -6,7 +6,7 @@ class SchedulerConfig(object):
     JOBS = [
         {
             'id': 'db_connect_job',
-            'func': 'jobs.timer:db_connecter',
+            'func': 'jobs.tasks.timer:db_connecter',
             'args': None,
             'trigger': 'interval',
             'seconds': 5*3600
