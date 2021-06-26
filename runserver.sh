6c2f108883443b34c209298324510317d86c5f11
WengChaoxi <352120190@qq.com>
2021-06-26 20:04:10 +0800

    update

--- runserver.sh
+++ runserver.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if [ $1 = "start" ]; then
+if [ "$1" = "start" ]; then
     echo -e "[runserver] starting server...\c"
     nohup uwsgi uwsgi/uwsgi.ini &>>logs/web_server.log &
     echo "success"
-elif [ $1 = reload ]; then
+elif [ "$1" = reload ]; then
     echo -e "[runserver] reloading server...\c"
     uwsgi --reload uwsgi/uwsgi.pid
     echo "success"
-elif [ $1 = "stop" ]; then
+elif [ "$1" = "stop" ]; then
     echo -e "[runserver] stoping server...\c"
     uwsgi --stop uwsgi/uwsgi.pid
     echo "success"
